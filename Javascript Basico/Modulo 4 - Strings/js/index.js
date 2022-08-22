c5ce217b128972902c7c8a530201ed7cd5be8d4a
Agustin Di Vincenzo <agustin.dv.2021@gmail.com>
2022-08-22 09:31:53 -0300

    Modulo 4 completado

---
+++ Javascript Basico/Modulo 4 - Strings/js/index.js
@@ -0,0 +1,5 @@
+const d = document
+
+let largeText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil hic ea soluta labore repudiandae deserunt officiis consequatur? Voluptatum, voluptate dolores!'
+
+d.getElementById('regex').onclick = () => window.open('https://regexr.com/')
